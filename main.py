ffc10992484baa9c2476f41469edc6e7402c86ba
WarmBlank3t <tyler.y.yu@gmail.com>
2022-02-26 02:09:19 -0800

    pattis is cool

--- main.py
+++ main.py
@@ -2,3 +2,29 @@ print('Tyler wuz here')
 print('testing testing')
 
 print('Gao Ge')
+
+class Pattis(object):
+
+    mood = 'dissatisfied'
+    age = 'undefined'
+    #stats
+    teaching_stat = '不错'
+    speed = 1
+    strength = 5
+    magic = 0
+    charisma = -1
+    intelligence = 100
+
+    def __init__():
+        print('Pattis engaged')
+
+    def change_mood(self, mood):
+        mood = 'dissatisfied'
+        self.mood = mood
+
+    def import_pattis():
+        print('im already imported into your sorry brain')
+    
+    
+
+
